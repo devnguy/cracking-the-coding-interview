5fe97505131d8cdef95131c140071f6a307d2f33
Devin Nguyen <hdn301@gmail.com>
2020-05-03 14:22:14 -0500

    follow up

--- 2-linked-lists/2.1_removeDups.js
+++ 2-linked-lists/2.1_removeDups.js
@@ -1,12 +1,9 @@
+/**
+ * Write code to remove duplicates from an unsorted linked list.
+ * Follow up: How would you solve this problem if a temporrary buffer was not
+ * allowed?
+ */
 const LinkedList = require('./LinkedList')
-
-const ll = new LinkedList()
-ll.add(2)
-ll.add(2)
-ll.add(1)
-ll.add(1)
-ll.add(3)
-ll.add(3)
 
 const removeDuplicates = (ll) => {
   const duplicates = {}
@@ -17,20 +14,41 @@ const removeDuplicates = (ll) => {
     } else {
       duplicates[current.data]++
     }
-    current = current.next
-  }
-  ll.display()
-
-  current = ll.head
-  while (current) {
     if (duplicates[current.data] > 1) {
       ll.remove(current.data)
       duplicates[current.data]--
     }
     current = current.next
   }
-  ll.display()
-  console.log(duplicates)
 }
 
-removeDuplicates(ll)
+const removeDuplicates2 = (ll) => {
+  let current = ll.head
+  while (current) {
+    let forwardPtr = current.next
+    while (forwardPtr) {
+      if (forwardPtr.data === current.data) {
+        ll.remove(forwardPtr.data)
+        current = current.next
+      }
+      forwardPtr = forwardPtr.next
+    }
+    current = current.next
+  }
+}
+
+const ll = new LinkedList()
+ll.add(1)
+ll.add(2)
+ll.add(2)
+ll.add(3)
+ll.add(3)
+ll.add(4)
+ll.add(3)
+ll.add(4)
+ll.add(4)
+ll.add(4)
+
+ll.display()
+removeDuplicates2(ll)
+ll.display()
